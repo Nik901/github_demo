905bc250c6d5529d4200169393c09196c9778a72
Nik901 <44324608+Nik901@users.noreply.github.com>
2021-07-08 14:28:35 +0530

    initial commit of math.py file

---
+++ math.py
@@ -0,0 +1,8 @@
+#add function def add(x,y):
+    return x+y#subtract function
+def subtract(x,y):
+    return (x-y)    #multiply function    
+def multiply(x,y):
+    return (x*y)  # on master branch    #divide  added 
+def divide(x,y):
+    return x/y
